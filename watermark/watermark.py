284e37fa337736879e615a870f68fddd6921f2b9
leizingyiu <youcanfindme@126.com>
2021-11-10 11:26:34 +0800

    update

---
+++ watermark/watermark.py
@@ -0,0 +1,63 @@
+import os,traceback
+from PIL import Image
+
+dirname, filename = os.path.split(os.path.abspath(__file__))
+realPath=os.path.realpath(__file__)
+
+
+def get_folder(fpath,wm_file,save_path):
+    try:
+        img_suffix_list = ['png', 'jpg', 'bmp']
+        for i in os.listdir(fpath):
+            if i.split('.')[-1] in img_suffix_list:
+                img_path = fpath + '/' + i
+                img_water_mark(img_file=img_path,wm_file=wm_file,save_path=save_path)
+    except Exception as e:
+        print(traceback.print_exc())
+
+def img_water_mark(img_file, wm_file,save_path):
+    img = Image.open(img_file)   
+    watermark = Image.open(wm_file)   
+    img_size = img.size
+    wm_size = watermark.size
+        
+    if(img_size[0]>img_size[1]):
+        proportion=(img_size[0])/wm_size[0]*1.5
+    else:
+        proportion=(img_size[1])/wm_size[1]*1.5
+      
+
+    wm_newSize=tuple([int(wm_size[0]*proportion),int(wm_size[1]*proportion)])
+    watermark=watermark.resize(wm_newSize,resample=Image.BICUBIC,box=None,reducing_gap=None)
+    wm_position = (int(img.size[0]/2-watermark.size[0]/2),int(img.size[1]/2-watermark.size[1]/2))
+    
+    print('-'*10)
+    print('img.size：',img.size)
+    print('proportion',proportion)
+    print('watermark.size：',watermark.size)
+
+    layer = Image.new('RGBA', img.size) 
+    layer.paste(watermark, wm_position) 
+    mark_img = Image.composite(layer, img, layer)
+    new_file_name = '/new_'+img_file.split('/')[-1]
+    print(save_path + new_file_name)
+    mark_img.save(save_path + new_file_name)
+
+
+if __name__ == "__main__":
+    print('input:\t\t',dirname+'/pic\noutput:\t\t',dirname+'/save\nwatermark:\t',dirname+'/watermark.png' )
+    print('by leizingyiu')
+    
+    if not os.path.exists(dirname+'/pic'):
+        print('找不到pic文件夹，请将需要添加水印的图片，放到pic文件夹')
+    else:
+        if not os.path.exists(dirname+'/save'):
+            os.mkdir(dirname+'/save')
+        if not os.path.exists(dirname+'/watermark.png'):
+            print('找不到watermark.png文件，请将水印文件命名为watermark.png')
+        else:
+            get_folder(dirname+'/pic',dirname+'/watermark.png',dirname+'/save')
+
+        input('处理完成，按任意键退出')
+
+
---
+++ watermark_无pic示例文件夹，需自己新建/readme.txt
@@ -0,0 +1,34 @@
+win
+下载文件，解压
+
+附件: watermark.zip  https://pan.baidu.com/s/1RgcuDMkzqtsmQ8kByrrEnw 提取码: k43k 
+
+
+将要加水印的文件放到 [自己新建] 的pic文件夹
+图片: https://uploader.shimo.im/f/XGbgizPwp1OBdDBA.png?sm_xform=image%2Fcrop%2Cx_0%2Cy_20%2Cw_267%2Ch_88&accessToken=eyJhbGciOiJIUzI1NiIsImtpZCI6ImRlZmF1bHQiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhY2Nlc3NfcmVzb3VyY2UiLCJleHAiOjE2MzY1MTQwNzgsImciOiJ2M0toeEpjdkR0UFBEaDZoIiwiaWF0IjoxNjM2NTEzNzc4LCJ1c2VySWQiOjM1ODIzNjN9.DxhW4zosPGVcWEVvCO5zXM1vlk2noAaUtz5w5lbPutU
+
+
+双击watermark.exe
+
+
+完成
+图片: https://uploader.shimo.im/f/beBBaZWhtFg1JzFk.png?sm_xform=image%2Fcrop%2Cx_0%2Cy_20%2Cw_299%2Ch_632&accessToken=eyJhbGciOiJIUzI1NiIsImtpZCI6ImRlZmF1bHQiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhY2Nlc3NfcmVzb3VyY2UiLCJleHAiOjE2MzY1MTQwNzgsImciOiJ2M0toeEpjdkR0UFBEaDZoIiwiaWF0IjoxNjM2NTEzNzc4LCJ1c2VySWQiOjM1ODIzNjN9.DxhW4zosPGVcWEVvCO5zXM1vlk2noAaUtz5w5lbPutU
+
+_________________
+
+
+mac
+python.org 下载python稳定版
+python.org
+
+使用pip安装Pillow
+sudo pip3.x install Pillow -i https://pypi.douban.com/simple/
+
+
+将需要添加水印的图片放到pic文件夹
+将水印命名为watermark.png
+在IDLE中打开py文件，按f5运行
+
+
+
+
